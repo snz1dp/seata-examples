f524f82e2a7f34cb354f50ee8e03ca84b33f37d2
痞子飞猪 <13317312768@qq.com>
2021-03-11 22:28:59 +0800

    修改接口定义

--- account/src/main/java/com/snz1/seatas/account/api/AccountController.java
+++ account/src/main/java/com/snz1/seatas/account/api/AccountController.java
@@ -5,7 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 
 import com.snz1.seatas.account.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,13 +16,13 @@ import java.math.BigDecimal;
 @RestController
 public class AccountController {
 
-    @Autowired
-    AccountService accountService;
+  @Autowired
+  AccountService accountService;
 
-    @GetMapping
-    public void debit(@RequestParam String userId, @RequestParam BigDecimal orderMoney) {
-        log.info("account XID = {}", RootContext.getXID());
-        accountService.debit(userId, orderMoney);
-    }
+  @PostMapping(value = "{userId}")
+  public void debit(@PathVariable("userId") String userId, @RequestParam BigDecimal orderMoney) {
+    log.info("account XID = {}", RootContext.getXID());
+    accountService.debit(userId, orderMoney);
+  }
 
 }
